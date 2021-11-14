c49bffcf0b27e8534e432be44eaceec731ab7ed1
Ruta <rutakalytyte@Ruta-Kalytyte-MacBook-Pro.local>
2021-11-14 19:45:39 +0200

    line of random numbers

---
+++ script.js
@@ -0,0 +1,33 @@
+let sudoku = [];
+let consoledSudoku = '';
+
+function generateRandomNumber() {
+	return Math.floor(Math.random() * 9 + 1);
+}
+
+function createSudokValues() {
+	for (let x = 1; x <= 9; x++) {
+		var iterationsAmount = 0;
+		var randomNumber = generateRandomNumber();
+
+		if (!sudoku.includes(randomNumber)) {
+			sudoku.push(randomNumber);
+		} else {
+			while (iterationsAmount <= 10) {
+				randomNumber = generateRandomNumber();
+
+				if (!sudoku.includes(randomNumber)) {
+					sudoku.push(randomNumber);
+					break;	
+				}
+
+				iterationsAmount += 1;
+			}
+		}
+	}
+
+	return sudoku;
+}
+
+console.log(createSudokValues());
+
