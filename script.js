96b7fcecd8b7dd0c58917f0322d5f2c22fdb9a53
Ruta <rutakalytyte@Ruta-Kalytyte-MacBook-Pro.local>
2021-11-15 21:14:02 +0200

    .

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-let sudoku = [];
-let row = [];
+var row = [];
+var sudoku = [];
+var fullSudoku = [];
 
 function generateRandomNumber() {
 	return Math.floor(Math.random() * 9 + 1);
@@ -8,6 +9,7 @@ function generateRandomNumber() {
 function createSudokValues() {
 	for (let y = 1; y <= 9; y++) {
 		for (let x = 1; x <= 9; x++) {
+			console.log ("Y ", y , "X ", x)
 			let iterationsAmount = 0;
 			let randomNumber = generateRandomNumber();
 			var isDigitExistentInRow = row.some(square=> square.digit === randomNumber);
@@ -40,12 +42,19 @@ function createSudokValues() {
 				}
 			}
 
-			var fullSudoku = sudoku.concat(row);
-			// inner for
-		}
+			// inner for end
 
-	return fullSudoku;
+
+		}
+		console.log("FULL ROW ", row)
+		//fullSudoku = sudoku.concat(row);
+		fullSudoku = sudoku.push(...row);
+		console.log("FULL SUDOKU ", fullSudoku)
+		// HERE fullSudoku value is overwriten every time
+		row = [];
+	// outer for end
 	}
+	return fullSudoku;
 }
 
 console.log(createSudokValues());
